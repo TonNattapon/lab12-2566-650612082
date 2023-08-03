a6d1448ee3a418c848771f910e7b8689344fdf33
= <=>
2023-08-03 15:20:35 +0700

    Completed

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,19 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
 import { useContext } from "react";
+import { LangContext } from "@/contexts/LangContext";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
+
+  const detailLabels =
+    theme.name === "light"
+      ? lang.detail.lightActivated
+      : lang.detail.darkActivated;
+
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
-      Display activated theme message here (EN or TH)
+      {detailLabels}
       {/* tip: Get information from "theme" variable to determine 
       which theme is currently selected */}
     </p>
